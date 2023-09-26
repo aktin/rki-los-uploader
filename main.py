94d3f7d2b8374dba4cc7f64114e5bb6521530be8
whoy <whoy@ukaachen.de>
2023-09-26 15:23:16 +0200

    Added readme for using the extraction tool and added some comments in the main code

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import datetime
-import shutil
-import time
-
 import requests
 import re
 import zipfile
-import unittest
 
 headers = {'Authorization': 'Bearer xxxAdmin1234'}
 zip_name = 'export_data_cache.zip'
 
 
-def request_ids_from_broker(broker_url: str):
+def request_ids_from_broker():
     url = "https://aktin-test-broker.klinikum.rwth-aachen.de/broker/request"
     response = requests.request("GET", url, headers=headers)
 
@@ -23,7 +18,7 @@ def extract_request_ids(raw_data: str):
     return ids
 
 
-def get_zip_from_id(_id: int, dest_url):
+def create_zip_from_id(_id: int, dest_url):
     # get resource identifier
     uuid_request_url = f"https://aktin-test-broker.klinikum.rwth-aachen.de/broker/export/request-bundle/{_id}"
     uuid = requests.request("POST", url=uuid_request_url, headers=headers).text
@@ -41,21 +36,34 @@ def get_zip_from_id(_id: int, dest_url):
 
 
 def get_case_data_from_zip(zip_dir, zip_name):
+    # open export zip file
     with zipfile.ZipFile(f"{zip_dir}/{zip_name}") as zf:
         for filename in zf.namelist():
             match = re.findall(r'(\d+)_result.zip', filename)
+
+            # if zip contains a result sub zip directory...
             if len(match) > 0:
                 zf.extract(filename, zip_dir)
+
+                # ... extract the case_data.txt from it
                 with zipfile.ZipFile(f"{zip_dir}{filename}") as result_dir:
+                    # Todo handle keyerror when no case data exists
                     result_dir.extract('case_data.txt', zip_dir)
 
 
+# gets case data from export with the highest id value
+def get_latest_case_data():
+    _id_list_raw = request_ids_from_broker()
+    _id_list = extract_request_ids(_id_list_raw)
+    _id = max(_id_list)
+
+    zip_dir, zip_name = create_zip_from_id(_id=_id, dest_url=f'cache/exports/')
+    get_case_data_from_zip(zip_dir, zip_name)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # raw = request_ids_from_broker("https://aktin-test-broker.klinikum.rwth-aachen.de/broker/request")
-    # id_list = extract_request_ids(raw)
-    zip_dir, zip_name = get_zip_from_id(1390, f'cache/exports/')
-    get_case_data_from_zip(zip_dir, zip_name)
+    get_latest_case_data()
+
 
 # class TestClass(unittest.TestCase):
 #     def test_get_request_ids(self):
---
+++ readme.md
@@ -0,0 +1,2 @@
+### Operation
+To download and extract the latest "case_data" from an export, run: " get_latest_case_data()". it will be downloaded and extracted in the following directory in this project: "cache/exports"
